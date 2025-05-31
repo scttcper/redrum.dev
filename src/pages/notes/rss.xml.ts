b89d83408323f7d1c366b3b9ab96236fd894430f
Scott Cooper <scttcper@gmail.com>
2025-05-31 15:45:22 -0700

    add back rss feeds, full content in feeds

--- src/pages/notes/rss.xml.ts
+++ src/pages/notes/rss.xml.ts
@@ -1,6 +1,10 @@
 import { getCollection } from 'astro:content';
 import { siteConfig } from '@/site.config';
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export const GET = async () => {
   const notes = await getCollection('note');
@@ -11,6 +15,10 @@ export const GET = async () => {
     site: import.meta.env.SITE,
     items: notes.map((note) => ({
       title: note.data.title,
+      description: note.data.description || '',
+      content: sanitizeHtml(parser.render(note.body || ''), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
       pubDate: note.data.publishDate,
       link: `notes/${note.id}/`,
     })),
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import { getAllPosts } from '@/data/post';
 import { siteConfig } from '@/site.config';
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export const GET = async () => {
   const posts = await getAllPosts();
@@ -12,6 +16,9 @@ export const GET = async () => {
     items: posts.map((post) => ({
       title: post.data.title,
       description: post.data.description,
+      content: sanitizeHtml(parser.render(post.body ?? ''), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
       pubDate: post.data.publishDate,
       link: `posts/${post.id}/`,
     })),
